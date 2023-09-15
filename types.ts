d74847a9af6ea479dd13ea5f35ae0ef23b2c854d
dstarr25 <91504587+dstarr25@users.noreply.github.com>
2023-09-15 01:24:39 -0400

    types changes

--- types.ts
+++ types.ts
@@ -35,6 +35,12 @@ export enum CloseReasons {
     NAME_TAKEN = 4002,
 }
 
+export const CodeMessages = {
+    [CloseReasons.GAME_NO_EXIST]: 'The game you are trying to join does not exist.',
+    [CloseReasons.GAME_IN_PROGRESS]: 'The game you are trying to join is in progress.',
+    [CloseReasons.NAME_TAKEN]: 'The name you have chosen was already taken.',
+}
+
 export enum GameStates {
     OPEN = 'open',
     PROMPTS = 'prompts',
